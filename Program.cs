04f80f2a50bf8eb78f18fd0ae4a407d60862af46
synzr <synzr@klo.pp.ru>
2026-04-28 01:52:59 +0500

    добавлено хостинг с AppHostedService

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TopLibraryManager.Services;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.SetBasePath(Directory.GetCurrentDirectory());
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+    })
+    .ConfigureServices(services =>
+    { 
+        services.AddHostedService<AppHostedService>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ Services/AppHostedService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Text;
+
+namespace TopLibraryManager.Services;
+
+public class AppHostedService(IHostApplicationLifetime appLifetime) : BackgroundService
+{
+    private readonly IHostApplicationLifetime _appLifetime = appLifetime;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Привет мир!");
+
+        _appLifetime.StopApplication();
+    }
+}
